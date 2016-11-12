bb1eb5b0ef244bd4abdfee1b50f531f529cfa434
bchok <bchok91@gmail.com>
2016-11-12 13:47:07 -0500

    added in decorator design patter to alter the length of the stick

--- project1_program1.php
+++ project1_program1.php
@@ -6,15 +6,18 @@
     }
 
     //class for hockey stick that implements stick 
+    //it also extends stickLength which is an abstract class for my stickDecorator
     //this allows for the call to the public createStick function 
     //while also allowing it to be customized based on the fact that it is a hockey stick
-    class Hockey implements Stick{
+    class Hockey extends stickLength implements Stick{
 
-        private $curve;
+        public $length = 0;
 
-        public function _construct($cur){
-            $this->curve = $cur;
+        //returns the sticklength so it can be altered by my decorator class
+        public function getStickLength(){
+            return $this->length;
         }
+        
 
         public function createStick(){
             echo "Creating a hockey stick!";
@@ -22,14 +25,16 @@
     }
 
     //class for a lacrosse stick that implements stick 
+    //it also extends stickLength which is an abstract class for my stickDecorator
     //this allows for the call to the public createStick function 
     //while also allowing it to be customized based on the fact that it is a lacrosse stick
-    class Lacrosse implements Stick{
+    class Lacrosse extends stickLength implements Stick{
 
-        private $length;
+        public $length = 0;
 
-        public function _construct($len){
-            $this->length = $len;
+        //returns the sticklength so it can be altered by my decorator class
+        public function getStickLength(){
+            return $this->length;
         }
 
         public function createStick(){
@@ -60,5 +65,52 @@
 
     $lacrosseStick = $factory->create("Lacrosse");
     echo $lacrosseStick->createStick();
+
+
+    //abstract class that holds the function getStickLength 
+    //it is abstract because it does not actually call the function
+    abstract class stickLength{
+        abstract function getStickLength();
+    }
+
+    //decorator class that extends stickLength so that it has access to getStickLength()
+    //it is abstract because it does not actually call getSticklength() itself
+    abstract class StickDecorator extends stickLength{
+        protected $stickLength;
+        function _construct(stickLength $stickLength){
+            $this->stickLength = $stickLength;
+        }
+    }
+
+    //decorator class for hockey stick that adds onto the stickLength and returns it 
+    //via getStickLength 
+    //number I picked is arbitrary at 155 to prove it works
+    class hockeyStickDecorator extends StickDecorator{
+        function getStickLength(){
+            return $this->stickLength->getStickLength+155;
+        }
+    }
+
+    //decorator class for lacrosse stick that adds onto the stickLength and returns it 
+    //via getStickLength 
+    //number I picked is arbitrary at 155 to prove it works
+    class lacrosseStickDecorator extends StickDecorator{
+        function getStickLength(){
+            return $this->stickLength->getStickLength+60;
+        }
+    }
+
+    //test objects to show that the decorator works
+    //each object creates a new decorator object while 
+    //creating the original through the stickFactory
+    $hStickTest = new hockeyStickDecorator(new Hockey());
+    echo $hStickTest->getStickLength();
+    $lStickTest = new lacrosseStickDecorator(new Lacrosse());
+    echo $lStickTest->getStickLength();
+
+
+    
+
+
 
 ?>
