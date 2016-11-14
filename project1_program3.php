b74d32ba3a6713e10db50db0e27a980fdf5edabf
bchok <bchok91@gmail.com>
2016-11-14 16:09:55 -0500

    attempt at implementing a factory with a strategy pattern, somewhere along the lines my code ended up broken.  It isn't a syntax error but logic and I cannot figure out what is wrong with it

---
+++ project1_program3.php
@@ -0,0 +1,57 @@
+<?php
+
+    
+    interface StringOutputter{
+        public function getStringValue();
+    }
+    //class to facilitate creation of strings
+    class StringCreator implements StringOutputter{
+        private $stringoutput;
+
+        //echos that a new string is being created
+        public function getStringValue(){
+            echo "new string is being created";
+        }
+       public function setOutput(StringOutputInterface $outputType){
+            $this->stringoutput = $outputType;
+        }
+        public function loadOutput(){
+            return $this->stringoutput->load();
+        }
+    }
+
+    //factory that calls stringcreator to create new strings
+    class StringFactory{
+        public static function create(){
+            return new StringCreator();
+        }
+    }
+
+    $test = new StringFactory();//creates new string through the factory
+    echo $test->getStringValue();//shows a string factory object proving it was created
+
+    //strategy interface that will load differenent variations of the string
+    interface StringOutputInterface{
+        public function load();
+    }
+
+    //class that implements the interface to uppercase the entire string
+    class UpperCasedString implements StringOutputInterface{
+        public function load(){
+            return strtoupper($somestring);
+        }
+    }
+    //class that implements the interface to lowercase the entire string
+    class LowerCasedString implements StringOutputInterface{
+        public function load(){
+            return strtolower($somestring);
+        }
+    }
+
+    $test->setOutput(new UpperCasedString());
+    echo $test->loadOutput();**/
+
+    
+
+
+?>
