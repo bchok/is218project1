d4f49ab0bbcbea3c018c47cdb22bff796eb90700
bchok <bchok91@gmail.com>
2016-11-18 08:37:53 -0500

    fixed my singleton constructor and adapter pattern class.  Neatened up the web display and fixed comments on code

--- project1_program2.php
+++ project1_program2.php
@@ -1,10 +1,11 @@
 <?php
-
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
     class Settings {
         private $setting = array(); //allows no other class to access the array
         private static $instance; //allows no other class to utlize the instance variable
 
-        private function _construct(){} //private constructor means no one can instantiate an object from the client
+        private function __construct(){} //private constructor means no one can instantiate an object from the client
 
         //creates an instance of the object if one does not already exist
         public static function getInstance(){
@@ -30,22 +31,14 @@
 
     //shows that the setting stays in tact after remove the reference above and referencing the already created instance from another variable
     $set2 = Settings::getInstance();
+    echo 'This proves that my singleton object was created<br>';
     echo $set2->getSettings("color");
 
     //proxy class allows settings to be intialized from outside the class
     class SettingsProxy extends Settings{
-        private $isChosen = false; //this indicates if settings have been chosen
-        private $isInitialized = false; //this indicates if the object has been intialized
+        private $settings = NULL;
 
-        //this accesses the parent constructor of the class it extends 
-        //in this case settings
-        public function _construct(array $setting){
-            parent::_construct($setting);
-            
-             if (count($setting) > 0){
-                $this->isInitialized = true; //if something is in the array it will return true
-                $this->isChosen = true; //if something is already in the array it will return true
-            }
+        public function __construct(){
         }
         //allows you to set settings from the proxy using the same parent function
         public function setSettings($key, $val){
@@ -59,32 +52,30 @@
     }
 
     $set3 = new SettingsProxy(); //creates a new proxy
-    $set3->setSettings("color", "blue"); //sets the settings 
+    $set3->setSettings("color", "blue"); //sets the settings
+    $set3->setSettings("resolution", "720");
+    echo '<br><br> This proves that a singleton object was created through my proxy.. 1 means a boolean true<br>';
     echo $set3->isChosen();//returns true or in this case 1 if settings are set
-    echo $set3->getSettings("color"); //returns the value from the key color to show its working with my singleton 
+    echo '<br><br> This proves that the singleton object created by the proxy can be called for output<br>';
+    echo $set3->getSettings("color"); //returns the value from the key color to show its working with my singleton
 
-    //adapter that extends settings proxy 
+    //adapter that extends settings proxy
     //it should adapt what SettingsProxy inherits to apply it to new settingsadapter objects
     class SettingsAdapter extends SettingsProxy{
-        private $setting;
+        private $proxy;
 
-        //this pulls applies the setting in a similar way to the proxy
-        public function _construct(array $setting){
-            $this->setting = $setting;
-            parent::_construct($setting); //calls the parent class through the inheritance tree to construct the object
+        //this takes in my proxy object and sets it to the variable of the adapter class
+        public function __construct(SettingsProxy $proxyIn){
+            $this->proxy = $proxyIn;
         }
-        //allows access to settings the same way as the proxy
-        public function getSettings($key){
-            return $this->setting[$key];
-        }
-        //allows access to set settings the same way as the proxy
-        public function setSettings($key, $val){
-            parent::setSettings($key, $val);
+        //this retrieves the settings from the proxy object and sets it as the settings of the adapter object
+        public function getSettings($key1, $key2){
+            return "This color: " .$this->proxy->getSettings($key1). " with this resoltion: " .$this->proxy->getSettings($key2);
         }
     }
-    
+
     //test values
-    $set4 = new SettingsAdapter();
-    $set4->setSettings("color", "green");
-    echo $set4->getSettings("color");
+    $settingAdapter = new SettingsAdapter($set3);//passes my proxy through the adapter
+    echo '<br><br> This proves that my adapter pattern works after passing the proxy object through it<br>';
+    echo $settingAdapter->getSettings("color", "resolution");
 ?>
