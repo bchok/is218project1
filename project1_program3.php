fb01da2ff2a9eb0fe8957ee6c9fdc53c44eb109e
bchok <bchok91@gmail.com>
2016-11-17 19:33:21 -0500

    added comments and neatened up the code for the program

--- project1_program3.php
+++ project1_program3.php
@@ -3,32 +3,42 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+  //this is the concrete class that the factory utlizes to create some random strings
+  //it implements my string decorater to append a string to the end of the user string
   class StringCreator extends stringAlterer{
     private $someString;
 
+    //constructs the string based on user input
     public function __construct($someString){
       $this->someString = $someString;
     }
+    //returns the string
     public function getString(){
       return $this->someString;
     }
+    //needed to satisfy the requirements of the abstract function in my strategy pattern
     public function getNewString(){
       return $this->someString;
     }
   }
 
+  //this is the actual factory class that calls the concrete string creator
   class StringFactory{
     public static function create($someString){
       return new StringCreator($someString);
     }
   }
 
+  //test values to the factory pattern
   $test = StringFactory::create("hello world");
   $test2 = StringFactory::create("TEST UPPER STRING");
-  //print_r($test->getString());
-  echo $test->getString();
-  echo $test2->getString();
+  echo $test->getString(); //outputs the string for test
+  echo $test2->getString(); //outputs the string for test2
 
+  //this was another part of the program I tried to create to allow me to choose a strategy
+  //solely based on user input rather than calling the strategy
+  //it was not required for the strategy pattern and i could not get it to work right so
+  //i commented it out
   /**class StrategyContext{
     private $strategy = NULL;
 
@@ -50,37 +60,44 @@ ini_set('display_errors', 1);
     }
   }**/
 
+  //strategy interface
   interface StrategyInterface{
-    public function showString($someString);
+    public function showString($someString); //function that the strategy utilizes
   }
 
+  //class that takes a string and uppercases it all
+  //it implements my strategy interface
   class StrategyUpper implements StrategyInterface{
     public function showString($someString){
-      $stratString = $someString->getString();
-      return strtoupper($stratString);
+      $stratString = $someString->getString();//takes the user string from the factory
+      return strtoupper($stratString);//uppercases the string
     }
+
   }
 
+  //class that takes a string and lowercases it all
+  //it implements my strategy interface
   class StrategyLower implements StrategyInterface{
     public function showString($someString){
-      $stratString = $someString->getString();
-      return strtolower($stratString);
+      $stratString = $someString->getString(); //takes user string from factory
+      return strtolower($stratString); //lowercases it
     }
   }
 
-  $stratU = new StrategyUpper();
-  echo $stratU->showString($test);
+  //test values for the strategy
+  $stratU = new StrategyUpper(); //creates new strategy
+  echo $stratU->showString($test); //takes factory object and alters it based on the strategy
+  $stratL = new StrategyLower(); //crates new strategy
+  echo $stratL->showString($test2); //takes factory object and alters it based on the strategy
 
-  $stratL = new StrategyLower();
-  echo $stratL->showString($test2);
-  //$test2Storage = $stratL->showString($test2);
 
-  echo 'Done';
-
+  //abstract class that my decorator uses
   abstract class stringAlterer{
-    abstract function getNewString();
+    abstract function getNewString(); //returns the new string decorated
   }
 
+  //the actual decorator class that constructs a new string for the decorator based on the object passed in
+  //my string creator extends this allowing the object to be passed in
   abstract class stringDecorator extends stringAlterer{
     protected $stringAlterer;
     function __construct(stringAlterer $stringAlterer){
@@ -88,20 +105,24 @@ ini_set('display_errors', 1);
     }
   }
 
+  //decorator class that appends a string to the end of the uppercased string
   class upperStringDecorator extends stringDecorator{
     function getNewString(){
       return $this->stringAlterer->getNewString(). " :This upper cased string has been decorated";
     }
   }
 
+  //decorator class taht appends a string to the end of the lowercased string
   class lowerStringDecorator extends stringDecorator{
     function getNewString(){
       return $this->stringAlterer->getNewString(). " :This lower cased string has been decorated";
     }
   }
 
-  $sDecTest = new upperStringDecorator($test2);
-  echo $sDecTest->getNewString();
+  //test values for the decorator passing a string object through
+  $sDecTest = new lowerStringDecorator($test); //takes string object and passes it through the decorator
+  echo $sDecTest->getNewString(); //outputs the decorated string
+
 
 
 
